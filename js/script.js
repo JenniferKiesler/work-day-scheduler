0e2db652548f906cd5bad947a2161d7c96f31b15
JKies <jennyhawes24@gmail.com>
2022-09-27 20:58:13 -0500

    adds pseudocode

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// var for current day
+var currentDay = $('#currentDay')
+console.log(currentDay)
+// var for class container
+// var for time (current hour)
+
+// moment() to display current day 
+
+// create time blocks for 9am to 5pm
+    // 3 col
+        // col-2 div (time)
+        // col-8 textarea
+            // click on time block and enter an event
+        // col-2 div (save button)
+
+// color code time blocks to indicate past, present, or future
+    // past = grey
+    // present = red
+    // future = green
+
+// save button that saves text for that event in local storage
+    // text shows up at top saying the text was saved to local storage
