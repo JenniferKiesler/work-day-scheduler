d315ef05f9d8826456187add6913a82e7837d53f
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-09-28 18:32:29 -0500

    edits current hour format and timeBlocks[]

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 var currentDay = $('#currentDay')
 var calendar = $('.container')
-var hour = moment().format('h');
-var timeBlocks = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5];
+var hour = moment().format('ha');
+var timeBlocks = [
+    moment('8am', 'ha').format('ha'), 
+    moment('9am', 'ha').format('ha'), 
+    moment('10am', 'ha').format('ha'), 
+    moment('11am', 'ha').format('ha'), 
+    moment('12pm', 'ha').format('ha'), 
+    moment('1pm', 'ha').format('ha'), 
+    moment('2pm', 'ha').format('ha'), 
+    moment('3pm', 'ha').format('ha'), 
+    moment('4pm', 'ha').format('ha'), 
+    moment('5pm', 'ha').format('ha')
+];
+
 
 // display current day 
 currentDay.text(moment().format('dddd, MMMM Do'));
@@ -10,11 +22,7 @@ currentDay.text(moment().format('dddd, MMMM Do'));
 $(timeBlocks).each(function(index){
     var row = $('<div>').addClass('row time-block').appendTo(calendar);
 
-    if (index <= 3) {
-        $('<div>').addClass('col-1 hour').text(timeBlocks[index] + 'am').appendTo(row);
-    } else {
-        $('<div>').addClass('col-1 hour').text(timeBlocks[index] + 'pm').appendTo(row);
-    }
+    $('<div>').addClass('col-1 hour').text(timeBlocks[index]).appendTo(row);
     
     $('<textarea>').addClass('col-10 description').appendTo(row);
 
